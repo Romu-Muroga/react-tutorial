25ea217ae2fc376dda5ffbd9d06f0b6db480c275
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-10-28 18:36:14 +0900

    Stateのリフトアップ、再び

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import './index.css';
 // Square コンポーネントは制御されたコンポーネント (controlled component) になった
 // 関数コンポーネントに書き換え = render メソッドだけを有して自分の state を持たないコンポーネントを、よりシンプルに書くための方法
 function Square(props) {
-  // Board から value と onClick が渡されている
+  // Board から value と onClick を受け取る
   return (
-    // Board の this.handleClick(i) を呼び出す
+    // マス目がクリックされたら Board の this.props.onClick(i) を呼び出す
     <button className="square" onClick={props.onClick}>
       {props.value}
     </button>
@@ -16,63 +16,20 @@ function Square(props) {
 
 // Board(親) >>> Square(子)
 class Board extends React.Component {
-  // ゲームの状態を各 Square の代わりに親の Board コンポーネントで保持する
-  constructor(props) {
-    super(props);
-    this.state = {
-      // 9 個のマス目に対応する 9 個の null 値をセット
-      squares: Array(9).fill(null),
-      // どちらのプレイヤーの手番なのかを決める値をセット
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i) {
-    // const(定数)で定義しても配列の値は変更できる
-    // squares を直接変更する代わりに、.slice() を呼んで配列のコピーを作成している
-    // なぜ？ => 複雑な機能が簡単に実装できる・変更の検出・React の再レンダータイミングの決定
-    const squares = this.state.squares.slice();
-    // ゲームの決着が既についている場合やクリックされたマス目が既に埋まっている場合に return を返す
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    // i にはクリックした番号が渡されている
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      // プレイヤーを交代するため真偽値を反転させる
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
   renderSquare(i) {
-    // Square に props を渡す
     return (
       <Square
-        // それぞれの Square が value プロパティを受け取る
-        value={this.state.squares[i]}
-        // Square が Board の state を更新できるようにする必要がある
-        // state はそれを定義しているコンポーネント内でプライベートなものだから、
-        // Square から Board の state を直接書き換えることはできない
-        // 代わりに、Board から Square に関数を渡すことにして、
-        // マス目がクリックされた時に Square にその関数を呼んでもらうようにしましょう
-        onClick={() => this.handleClick(i)}
+        // Game から受け取った squares と onClick を、１つ１つの Square に渡す
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)} /* クリックされたタイミングで i に番号が代入される */
       />
     );
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
+      // 以下の renderSquare(i) から i が渡されている
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -93,15 +50,72 @@ class Board extends React.Component {
   }
 }
 
+// Game(親) >>> Board(子)
 class Game extends React.Component {
+  // ゲームの状態を各 Board の代わりに親の Game コンポーネントで保持する
+  constructor(props) {
+    super(props);
+    // state は状態を表す
+    this.state = {
+      history: [{
+        // 9 個のマス目に対応する 9 個の null 値をセット
+        squares: Array(9).fill(null),
+      }],
+      // どちらのプレイヤーの手番なのかを決める値をセット
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    // const(定数)で定義しても配列の値は変更できる
+    // squares を直接変更する代わりに、.slice() を呼んで配列のコピーを作成している
+    // なぜ？ => 複雑な機能が簡単に実装できる・変更の検出・React の再レンダータイミングの決定
+    const history = this.state.history; /* 全履歴 */
+    const current = history[history.length - 1]; /* 最新の履歴 */
+    const squares = current.squares.slice(); /* 最新の盤面をコピー */
+    // ゲームの決着が既についている場合やクリックされたマス目が既に埋まっている場合に return を返す
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    // i にはクリックしたマス目の番号が渡されている
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares, /* 最新の盤面の配列を履歴の配列(history)の末尾に結合 */
+      }]),
+      // プレイヤーを交代するため真偽値を反転させる
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   render() {
+    const history = this.state.history; /* 全履歴 */
+    const current = history[history.length - 1]; /* 最新の履歴 */
+    const winner = calculateWinner(current.squares); /* 最新の勝敗の判定 */
+
+    let status; /* state と違うので注意 */
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+    // Board に props を渡す
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares} /* 最新の盤面 */
+            // Square が Game の state を更新できるようにする必要がある
+            // state はそれを定義しているコンポーネント内でプライベートなものだから、
+            // Square から Game の state を直接書き換えることはできない
+            // 代わりに、Game から Board を経由して Square に関数を渡すことにして、
+            // マス目がクリックされた時に Square にその関数を呼んでもらうようにしましょう
+            onClick={(i) => this.handleClick(i)}
+           />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
